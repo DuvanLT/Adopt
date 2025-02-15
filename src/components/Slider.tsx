57e525ac9eb5da04c337dca006d85f4e38f4cb14
DuvanLT <duvanred0@gmail.com>
2025-02-15 08:38:35 -0500

    slider changes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,31 +1,50 @@
-import '../styles/slider.css'
-import { useChangeIndex } from '../hooks/useChangeIndex'
-import { SliderProps } from '../types/Slider.types'
-import { ChevronLeft,ChevronRight} from 'lucide-react'
-import { useTouch } from '../hooks/useTouch'
-
+import '../styles/slider.css';
+import { useChangeIndex } from '../hooks/useChangeIndex';
+import { SliderProps } from '../types/Slider.types';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useTouch } from '../hooks/useTouch';
 
 export default function Slider({ slides, SlideComponent }: SliderProps) {
-    const { currentIndex, nextSlide, prevSlide } = useChangeIndex(slides)
-    const {handleTouchStart,handleTouchMove,handleTouchEnd} = useTouch(nextSlide,prevSlide)
+    const { currentIndex, nextSlide, prevSlide } = useChangeIndex(slides);
+    const { handleTouchStart, handleTouchMove, handleTouchEnd } = useTouch(nextSlide, prevSlide);
+    const nextIndex = (currentIndex + 1) % slides.length;
+
     return (
-        <div 
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-        className="slider">
-            <ChevronLeft size={48} 
-             onClick={prevSlide} className="slider__button slider__button--left"  />
-             <div className="slider__content">
-            {SlideComponent && <SlideComponent slide={slides[currentIndex]} />}
-            <div className="slider__counter">
-            {slides.map((_,index) => (
-                <div key={index} className={`slider__counter-element ${index === currentIndex ? 'active' : ''}`}></div>
-            ))}
+        <div
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            className="slider"
+        >
+            <div className="slider__content">
+            <div className="slider__instructions">
+            <ChevronLeft
+                size={48}
+                onClick={prevSlide}
+                className="slider__button slider__button--left"
+            />
+            <ChevronRight
+                size={48}
+                onClick={nextSlide}
+                className="slider__button slider__button--right"
+            />
             </div>
+                {SlideComponent && (
+                    <>
+                        <div className="slider__current">
+                            <SlideComponent slide={slides[currentIndex]} />
+                            <div className="slider__counter">
+                            {slides.map((_,index) => (
+                                <div key={index} className={`slider__counter-element ${index === currentIndex ? 'active' : ''}`}></div>
+                            ))}
+                            </div>
+                        </div>
+                        <div className="slider__next">
+                            <SlideComponent slide={slides[nextIndex]} />
+                        </div>
+                    </>
+                )}
             </div>
-            <ChevronRight size={48} 
-             onClick={nextSlide} className="slider__button slider__button--right" />
         </div>
     )
 }
