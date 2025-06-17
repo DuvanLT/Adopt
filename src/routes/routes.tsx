6bad3049ebf9542ec6281cb5663893ed65b40450
DuvanLT <duvanred0@gmail.com>
2025-06-17 11:33:09 -0500

    solve bug

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,16 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.tsx"
 import Adopt from "../pages/Adopt.tsx";
 import Dogs from "../pages/Dogs.tsx";
 import Cats from "../pages/Cats.tsx";
 
 const AppRoutes = () => (
-    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/adopt" element={<Adopt />} />
-      <Route path="/dogs" element={<Dogs />} />
-      <Route path="/cats" element={<Cats />} />
+    <Route path="/" element={<Home />} />
+    <Route path="/adopt" element={<Adopt />} />
+    <Route path="/dogs" element={<Dogs/>} />
+    <Route path="/cats" element={<Cats/>} />
     </Routes>
-  </BrowserRouter>
 )
 
 export default AppRoutes
