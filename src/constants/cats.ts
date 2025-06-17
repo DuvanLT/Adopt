81439d1af4b457d4da201de96affba0ddcc37341
DuvanLT <duvanred0@gmail.com>
2025-06-16 21:57:19 -0500

    mascotas separadas y base de datos local archivos

---
+++ src/constants/cats.ts
@@ -0,0 +1,213 @@
+import { PetProps } from "../types/PetInformation.types"
+export const CATS: PetProps[]  = [
+ {
+    name: "Luna",
+    age: "3 años",
+    breed: "Gato Siamés",
+    birthday: "15/07/2020",
+    height: "pequeño",
+    likes: "Tomar el sol durante horas mientras observa aves",
+    dislikes: "Ruidos fuertes repentinos como truenos y aspiradoras",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/2/25/Siam_lilacpoint.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW1IdS2CLigVKb84B4FrSWcVKt0wQufXDlxw&s"
+    ]
+  },
+  {
+    name: "Whiskers",
+    age: "2 años",
+    breed: "Maine Coon",
+    birthday: "08/04/2021",
+    height: "grande",
+    likes: "Escalar torres para gatos y observar desde lugares altos",
+    dislikes: "Puertas cerradas que impiden el acceso a lugares interesantes",
+    image: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTyMMB6TIjctv7IWoNwjFG0P48-rQxK2x9qA&s",
+      "https://proquivetsas.com/wp-content/uploads/2025/02/el-precio-del-maine-coon-guia-completa-para-comprar-y-cuidar-este-gato.jpg"
+    ]
+  },
+  {
+    name: "Oliver",
+    age: "4 años",
+    breed: "Gato Persa",
+    birthday: "30/01/2019",
+    height: "mediano",
+    likes: "Sesiones diarias de cepillado con cepillos especiales",
+    dislikes: "El agua en cualquier forma – evita lavamanos, duchas y bebederos",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/White_Persian_Cat.jpg",
+      "https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-06Persian20Long20Hair.2.jpg?itok=qzNV_HuV"
+    ],
+    discapacity: "Dificultades respiratorias por braquicefalia"
+  },
+  {
+    name: "Milo",
+    age: "2 años",
+    breed: "Gato Bengalí",
+    birthday: "12/09/2021",
+    height: "mediano",
+    likes: "Jugar con grifos que gotean y tazones llenos de agua",
+    dislikes: "Ser sujetado o cargado contra su voluntad",
+    image: [
+      "https://www.shutterstock.com/image-photo/cute-bengal-cat-lying-bed-260nw-2086046251.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Paintedcats_Red_Star_standing.jpg/640px-Paintedcats_Red_Star_standing.jpg"
+    ]
+  },
+  {
+    name: "Cleo",
+    age: "5 años",
+    breed: "Sphynx",
+    birthday: "19/05/2018",
+    height: "pequeño",
+    likes: "Dormir bajo mantas térmicas y usar suéteres suaves",
+    dislikes: "Superficies ásperas que irritan su piel sensible",
+    image: [
+      "https://hankpets.com/wp-content/uploads/2025/01/History-of-the-Sphynx-Cat.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Sphinx2_July_2006.jpg/640px-Sphinx2_July_2006.jpg"
+    ],
+    discapacity: "Requiere rutina semanal de cuidado de la piel"
+  },
+  {
+    name: "Simba",
+    age: "1 año",
+    breed: "Munchkin",
+    birthday: "22/11/2022",
+    height: "pequeño",
+    likes: "Perseguir punteros láser y rodar pelotas por túneles",
+    dislikes: "Intentar saltos más allá de sus capacidades físicas",
+    image: [
+      "https://pethubusa.com/wp-content/uploads/2024/05/Munchkin-Cat-1-1024x640.jpg",
+      "https://st4.depositphotos.com/14841312/20745/i/450/depositphotos_207453654-stock-photo-kitty-cat-munchkin-fluffy-animal.jpg"
+    ]
+  },
+  {
+    name: "Chloe",
+    age: "6 años",
+    breed: "British Shorthair",
+    birthday: "10/10/2017",
+    height: "mediano",
+    likes: "Sentarse cerca de las ventanas y observar el mundo",
+    dislikes: "Ser levantada de forma inesperada",
+    image: [
+      "https://es.cats.com/wp-content/uploads/2020/10/British-Shorthair-cat.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Britishblue.jpg/640px-Britishblue.jpg"
+    ]
+  },
+  {
+    name: "Leo",
+    age: "4 años",
+    breed: "Ragdoll",
+    birthday: "05/03/2020",
+    height: "grande",
+    likes: "Acurrucarse en el regazo y relajarse al sol",
+    dislikes: "Aspiradoras y ambientes ruidosos",
+    image: [
+      "https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=1200,height=675,fit=cover/animal/breed/cat/adult/6672ad2546cf9525072129.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Ragdoll_from_Gatil_Ragbelas.jpg/640px-Ragdoll_from_Gatil_Ragbelas.jpg"
+    ]
+  },
+  {
+    name: "Misty",
+    age: "3 años",
+    breed: "Azul Ruso",
+    birthday: "17/08/2021",
+    height: "mediano",
+    likes: "Escalar árboles de gato suaves y observar aves",
+    dislikes: "Visitas y movimientos repentinos",
+    image: [
+      "https://www.zooplus.co.uk/magazine/wp-content/uploads/2018/03/russian-blue.webp",
+      "https://cdn0.uncomo.com/es/posts/7/0/2/como_es_el_gato_azul_ruso_22207_600.jpg"
+    ]
+  },
+  {
+    name: "Toby",
+    age: "2 años",
+    breed: "Scottish Fold",
+    birthday: "26/12/2022",
+    height: "pequeño",
+    likes: "Sentarse erguido como suricata y jugar con cuerdas",
+    dislikes: "Ruidos agudos y que le toquen las orejas",
+    image: [
+      "https://www.tiendanimal.es/articulos/wp-content/uploads/2022/01/Scottish-Fold-raza-1200x800.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Adult_Scottish_Fold.jpg/640px-Adult_Scottish_Fold.jpg"
+    ]
+  },
+  {
+    name: "Zoe",
+    age: "4 años",
+    breed: "Abisinio",
+    birthday: "03/06/2020",
+    height: "mediano",
+    likes: "Saltar de mueble en mueble y jugar a las atrapadas",
+    dislikes: "Estar confinada en una sola habitación",
+    image: [
+      "https://www.purelypetsinsurance.co.uk/media/xagpdsdc/abyssinian-cat-laying-down.jpg?format=webp",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1KXg4VLhi5Opzim19G7e7oiIbl3n31JQIBw&s"
+    ]
+  },
+  {
+    name: "Felix",
+    age: "5 años",
+    breed: "Turco Van",
+    birthday: "14/02/2019",
+    height: "grande",
+    likes: "Nadar en tinas poco profundas y jugar a traer objetos",
+    dislikes: "Electrodomésticos ruidosos y habitaciones llenas",
+    image: [
+      "https://tica.org/wp-content/uploads/2018/08/Turkish-Van-Full-Body.jpg",
+      "https://elquindiano.com/wp-content/uploads/2023/02/b202302270923488.jpg"
+    ]
+  },
+  {
+    name: "Lily",
+    age: "3 años",
+    breed: "Bosque de Noruega",
+    birthday: "11/04/2021",
+    height: "grande",
+    likes: "Dormir en lugares altos y rozarse con plantas",
+    dislikes: "Descensos bruscos de temperatura",
+    image: [
+      "https://www.thesprucepets.com/thmb/fMt9c_dfcVennGZ-fafrDm6JO2Y=/2121x0/filters:no_upscale():strip_icc()/GettyImages-1012874850-10aed4af5d554498acc283f55dab8742.jpg",
+      "https://animalxop.com/cdn/shop/articles/df42ee19-27ca-4ad1-a93c-19a0de6eb213.jpg?v=1702093718"
+    ]
+  },
+  {
+    name: "Gizmo",
+    age: "1 año",
+    breed: "Devon Rex",
+    birthday: "09/01/2023",
+    height: "pequeño",
+    likes: "Perseguir plumas y acurrucarse en suéteres",
+    dislikes: "Estar solo por mucho tiempo",
+    image: [
+      "https://www.zooplus.es/magazine/wp-content/uploads/2020/03/devon-rex-768x512.webp",
+      "https://www.petrebels.com/en/wp-content/uploads/sites/3/2023/03/kulli-kittus-mVDqIXh-dP8-unsplash-1-1024x683-1.jpg"
+    ]
+  },
+  {
+    name: "Peanut",
+    age: "2 años",
+    breed: "Oriental de Pelo Corto",
+    birthday: "07/09/2022",
+    height: "mediano",
+    likes: "Chillar a los insectos y esconderse en cajas",
+    dislikes: "Luces brillantes intermitentes",
+    image: [
+      "https://www.thesprucepets.com/thmb/Zq1eUigKcsL4fzDyQgL1qry4BQc=/1550x0/filters:no_upscale():strip_icc()/GettyImages-1324924369-5dfbda5801d44cfe927125d789b099a5.jpg",
+      "https://www.mascotarios.org/wp-content/uploads/2011/08/Oriental-pelo-corto.jpg"
+    ]
+  },
+  {
+    name: "Shadow",
+    age: "6 años",
+    breed: "Bombay",
+    birthday: "28/07/2018",
+    height: "mediano",
+    likes: "Acechar sombras en silencio y dormir bajo camas",
+    dislikes: "Ambientes concurridos y mascotas ruidosas",
+    image: [
+      "https://www.thesprucepets.com/thmb/RnGTM5ENlWu-D4oND397jksaUCw=/1911x0/filters:no_upscale():strip_icc()/bombay-cat-full-profile-history-and-care-5202250-hero-85810f454cf84a7786e650136a10f91c.jpg",
+      "https://www.patasencasa.com/sites/default/files/styles/article_detail_1200/public/2024-09/7_datos_curiosos_del_gato_bombay_0.jpg.webp?itok=Hr3TN5nk"
+    ]
+  }
+]
---
+++ src/constants/dogs.ts
@@ -0,0 +1,175 @@
+import { PetProps } from "../types/PetInformation.types"
+export const DOGS: PetProps[]  = [
+ {
+    name: "Mustang",
+    age: "4 años",
+    breed: "Pitbull",
+    birthday: "12/05/2021",
+    height: "mediano",
+    likes: "Jugar con prendas de ropa, especialmente calcetines y toallas – adora los juegos de tirar",
+    dislikes: "Evita el agua a toda costa, muestra incomodidad extrema durante los baños o en clima lluvioso",
+    image: [
+      "https://images.ctfassets.net/denf86kkcx7r/339lIrj1wg9bpirJBfHn3X/a2be2e619c8c6dea173b50b6b2c80356/pitbull_seguro_santevet-33",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/000_American_Pit_Bull_Terrier.jpg/330px-000_American_Pit_Bull_Terrier.jpg"
+    ],
+    discapacity: "Ceguera parcial en el ojo izquierdo"
+  },
+  {
+    name: "Rubik",
+    age: "2 años",
+    breed: "Labrador",
+    birthday: "22/02/2023",
+    height: "pequeño",
+    likes: "Disfruta absolutamente de todo: desde rompecabezas con comida hasta abrazos y aventuras al aire libre",
+    dislikes: "No tiene disgustos particulares, este compañero relajado encuentra alegría en todo",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/0/04/Labrador_Retriever_%281210559%29.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Labrador_on_Quantock_%282175262184%29.jpg/640px-Labrador_on_Quantock_%282175262184%29.jpg"
+    ]
+  },
+  {
+    name: "Max",
+    age: "5 años",
+    breed: "Golden Retriever",
+    birthday: "03/11/2018",
+    height: "grande",
+    likes: "Perseguir pelotas de tenis por horas y recoger palos en lagos",
+    dislikes: "Desarrolla ansiedad por separación cuando se queda solo por mucho tiempo",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Golden_Retriever_Dukedestiny01_drvd.jpg/640px-Golden_Retriever_Dukedestiny01_drvd.jpg"
+    ]
+  },
+  {
+    name: "Bella",
+    age: "1 año",
+    breed: "Bulldog Francés",
+    birthday: "20/10/2022",
+    height: "pequeño",
+    likes: "Acurrucarse bajo cobijas durante películas y estar en brazos",
+    dislikes: "Caminatas largas o ejercicio intenso debido a dificultades respiratorias",
+    image: [
+      "https://blog.dogfydiet.com/wp-content/uploads/2024/02/Caracteristicas-unicas-que-definen-al-Bulldog-Frances-Blanco-.jpg",
+      "https://www.zooplus.es/magazine/wp-content/uploads/2019/06/Bulldog-frances-2.webp"
+    ]
+  },
+  {
+    name: "Rocky",
+    age: "6 años",
+    breed: "Pastor Alemán",
+    birthday: "05/05/2017",
+    height: "grande",
+    likes: "Vigilar el perímetro del hogar y estar atento cerca de la familia",
+    dislikes: "Visitantes desconocidos o invitados inesperados activan su instinto protector",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Female_German_Shepherd.jpg/640px-Female_German_Shepherd.jpg"
+    ],
+    discapacity: "Displasia de cadera que requiere suplementos articulares"
+  },
+  {
+    name: "Daisy",
+    age: "3 años",
+    breed: "Beagle",
+    birthday: "22/03/2020",
+    height: "mediano",
+    likes: "Rastrear olores fascinantes durante los paseos por el vecindario",
+    dislikes: "La hora del baño – se esconde bajo los muebles al oír el agua",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/5/55/Beagle_600.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/MiloSmet.JPG/960px-MiloSmet.JPG"
+    ]
+  },
+  {
+    name: "Thor",
+    age: "4 años",
+    breed: "Husky Siberiano",
+    birthday: "17/08/2019",
+    height: "grande",
+    likes: "Tirar trineos por la nieve y aullar con la música",
+    dislikes: "Climas cálidos que le provocan sobrecalentamiento",
+    image: [
+      "https://kamanpet.cl/wp-content/uploads/2024/04/cual-es-el-mejor-alimento-para-un-husky-siberiano.webp",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Black-Magic-Big-Boy.jpg/640px-Black-Magic-Big-Boy.jpg"
+    ]
+  },
+  {
+    name: "Lola",
+    age: "2 años",
+    breed: "Border Collie",
+    birthday: "30/04/2021",
+    height: "mediano",
+    likes: "Resolver juguetes de lógica y aprender circuitos de agilidad",
+    dislikes: "Luces intermitentes o patrones visuales caóticos",
+    image: [
+      "https://www.zooplus.es/magazine/wp-content/uploads/2017/03/border-collie-im-grass.webp",
+      "https://rovinfood.com/wp-content/uploads/2024/05/portada-bordercollie-1.jpg"
+    ]
+  },
+  {
+    name: "Simba",
+    age: "3 años",
+    breed: "Shiba Inu",
+    birthday: "14/02/2021",
+    height: "pequeño",
+    likes: "Observar por la ventana y vigilar el patio con curiosidad",
+    dislikes: "Ser levantado de repente o encerrado en espacios pequeños",
+    image: [
+      "https://images.ctfassets.net/550nf1gumh01/5MHP2CZCatR7AIbFM1xlbr/34cffad9c1c535fd5fdd67cc9534fef2/iStock-1203500325.jpg?q=90&w=1240",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Taka_Shiba.jpg/640px-Taka_Shiba.jpg"
+    ]
+  },
+  {
+    name: "Nala",
+    age: "5 años",
+    breed: "Dálmata",
+    birthday: "10/06/2019",
+    height: "grande",
+    likes: "Correr junto a bicicletas y jugar en campos abiertos",
+    dislikes: "Las sirenas o fuegos artificiales le provocan inquietud",
+    image: [
+      "https://ambientlounge.cl/cdn/shop/articles/blog_3dda69bd-f653-4d87-8b26-8de572110c94.jpg?v=1690479436",
+      "https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-07Dalmatian2.jpg?itok=WgnY-HKT"
+    ]
+  },
+  {
+    name: "Otto",
+    age: "2 años",
+    breed: "Corgi",
+    birthday: "01/09/2022",
+    height: "pequeño",
+    likes: "Perseguir luces láser y cavar en tierra suave",
+    dislikes: "Quedarse atrás en caminatas o aventuras",
+    image: [
+      "https://www.worldlifeexpectancy.com//images/a/d/d/b/pembroke_welsh_corgi/pembroke_welsh_corgi_1.webp",
+      "https://nfnatcane.es/blog/wp-content/uploads/2021/02/Corgi-gales-marron-negro-y-blanco.jpg"
+    ]
+  },
+  {
+    name: "Bruno",
+    age: "4 años",
+    breed: "Rottweiler",
+    birthday: "29/12/2020",
+    height: "grande",
+    likes: "Compañía leal y masticar juguetes resistentes",
+    dislikes: "Personas extrañas que se acercan sin previo aviso",
+    image: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/02_I_Exposici%C3%B3n_Monogr%C3%A1fica_Club_Rottweiler_de_Espa%C3%B1a_-_Santa_Brigida_-_Gran_Canaria.jpg/640px-02_I_Exposici%C3%B3n_Monogr%C3%A1fica_Club_Rottweiler_de_Espa%C3%B1a_-_Santa_Brigida_-_Gran_Canaria.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Rottweiler_standing_facing_left.jpg/640px-Rottweiler_standing_facing_left.jpg"
+    ],
+    discapacity: "Pérdida leve de audición en un oído"
+  },
+  {
+    name: "Coco",
+    age: "1 año",
+    breed: "Pomerania",
+    birthday: "03/03/2023",
+    height: "pequeño",
+    likes: "Que lo carguen en brazos y posar para fotos",
+    dislikes: "Mojarse con la lluvia o ser cepillado de forma brusca",
+    image: [
+      "https://cdn.britannica.com/41/233841-050-4FFECCF1/Pomeranian-dog.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRar-oViUAF5fmWX5yc-S7A8dxTuANcG-CVjw&s"
+    ]
+  }
+]
