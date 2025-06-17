d4952740a3b47d35aea9ba79c40c3d36098b60e5
DuvanLT <duvanred0@gmail.com>
2025-06-17 11:06:14 -0500

    looking for the bug

--- src/pages/Adopt.tsx
+++ src/pages/Adopt.tsx
@@ -6,8 +6,8 @@ import { usePetContext } from "../context/petContext"
 import { PetProps } from "../types/PetInformation.types"
 import Footer from "../components/Footer"
 import { Link } from "react-router-dom"
-import ShowCardPets from "../components/ShowCardPets"
-import { DISCAPACITY_PETS, HEALTHY_PETS, PETS_SMALL } from "../constants/petCardToShow"
+//import ShowCardPets from "../components/ShowCardPets"
+//import { DISCAPACITY_PETS, HEALTHY_PETS, PETS_SMALL } from "../constants/petCardToShow"
 export default function Adopt() {
    const {setPetToAdopt,petToAdopt} = usePetContext()
    return(
@@ -18,9 +18,9 @@ export default function Adopt() {
    <Link to={"/dogs"}><Sesion image={"/pexels-rdne-7516476.jpg"} text={"Perros"} /></Link>
    <Link to={"/cats"}><Sesion image={"/pexels-shvetsa-4587958.jpg"} text={"Gatos"} /></Link>
     </div>
-    <ShowCardPets {...HEALTHY_PETS} onSelectPet={setPetToAdopt} />
+   {/* <ShowCardPets {...HEALTHY_PETS} onSelectPet={setPetToAdopt} />
     <ShowCardPets {...DISCAPACITY_PETS} onSelectPet={setPetToAdopt}/>
-    <ShowCardPets {...PETS_SMALL} onSelectPet={setPetToAdopt}/>
+    <ShowCardPets {...PETS_SMALL} onSelectPet={setPetToAdopt}/> */}
       {petToAdopt &&(
         <PetInformation {...(petToAdopt as PetProps)} />
       )}
